1b243c4292284a151d99cb21375fc339bdf5dfa2
GoHOME <alexela2@narod.ru>
2023-11-30 13:51:14 +0300

    Add IntToVisibilityConverter RadioButtonCheckedConverter converters

---
+++ Converters/IntToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿// -------------------------------------------------------------------------------------------------------------------------------------
+// <copyright company "HOME" file="IntToVisibilityConverter.cs">
+//  OrderOV
+// </copyright>
+// 
+// <summary>
+// create 07.02.2017  Ghost
+// </summary>
+//  -------------------------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CommonLib.Converters;
+
+[ValueConversion(typeof(int), typeof(bool))]
+public class IntToVisibilityConverter : IValueConverter
+{
+	public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		if (value == null || (int)value == 0)
+		{
+			return Visibility.Hidden;
+		}
+		return Visibility.Visible;
+	}
+
+	public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ Converters/RadioButtonCheckedConverter.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace CommonLib.Converters;
+
+/// <summary>
+/// This converter is used to bind a value to a group of radio buttons, and can be used with an enum, bool, int, etc.
+/// It depends on the converter parameter, which maps a radio button to a specific value.
+/// Note: this converter is not designed to work with flag enums (e.g. multiple checkboxes scenario).
+/// 
+/// Этот конвертер используется для связывания значение группы переключателей , и может быть использован с перечислениями BOOL , INT , и т.д.
+/// Это зависит от параметра преобразователя , который отображает радио-кнопку на определенное значение.
+/// Примечание : Этот конвертер не предназначен для работы с флагом перечисления ( например несколько checkboxes ) .
+/// 
+/// Here is an example of the bidning for an enum value:
+/// <RadioButton Content="Option 1" IsChecked="{Binding EnumValue, Converter={StaticResource EnumToRadioBoxCheckedConverter}, 
+///     ConverterParameter={x:Static local:TestEnum.Option1}}" GroupName="EnumGroup"/>
+/// <RadioButton Content="Option 2" IsChecked="{Binding EnumValue, Converter={StaticResource EnumToRadioBoxCheckedConverter}, 
+///     ConverterParameter={x:Static local:TestEnum.Option2}}" GroupName="EnumGroup"/>
+/// 
+///public enum TestEnum
+///{
+///    Option1,
+///    Option2,
+///    Option3
+///}
+/// </summary>
+public class RadioButtonCheckedConverter : IValueConverter
+{
+	public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		return value != null && value.Equals(parameter);
+	}
+
+	public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		return value != null && value.Equals(true) ? parameter : Binding.DoNothing;
+	}
+}
