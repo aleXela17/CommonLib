7f171b0fc499c8d3ed0dcd5b754e1161886aa30a
GoHOME <alexela2@narod.ru>
2023-11-30 13:52:23 +0300

    Add Base ViewModel

---
+++ ViewModel/Base/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace CommonLib.ViewModel.Base;
+
+public class ViewModel : INotifyPropertyChanged
+{
+	public static bool IsDesignMode { get; } = System.ComponentModel.DesignerProperties.GetIsInDesignMode(new DependencyObject());
+
+	#region INotifyPropertyChanged
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+
+	protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+	{
+		if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+		field = value;
+		OnPropertyChanged(propertyName);
+		return true;
+	}
+
+	#endregion
+}
---
+++ ViewModel/TitledViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonLib.ViewModel;
+
+public class TitledViewModel : Base.ViewModel
+{
+	/// <summary>Заголовок</summary>
+	private string? _title;
+
+	/// <summary>Заголовок</summary>
+	public string? Title { get => _title; set => SetField(ref _title, value); }
+}
